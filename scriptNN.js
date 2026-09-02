4f859069017e553247a4261d97d38f7792e37e3c
Eric Valderas <antikampa@gmail.com>
2026-09-02 07:59:21 +0200

    probando cositas

--- scriptNN.js
+++ scriptNN.js
@@ -184,7 +184,7 @@ function functionImgSerie10() {
 }
 
 function functionImgArcher1() {
-  window.open("https://nervouswreckradio.online/Marian/1.mkv");
+  window.open("https://drive.google.com/file/d/13ZOBEyS9eFq38AUePN7g3-jQ2RqbF3F9/view?usp=drive_link");
   ImgArcher1.style.border="5px solid red";
   localStorage.setItem("SaveImgArcherBorder1", "5px solid red");
 }
